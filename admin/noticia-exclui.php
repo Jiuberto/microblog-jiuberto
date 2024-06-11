4cd49b808cdefeadf7a2c327eed25960c6b5d97d
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-06-11 17:32:38 -0300

    excluirnoticia

---
+++ admin/noticia-exclui.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../inc/funcoes-noticias.php";
+require_once "../inc/funcoes-sessao.php";
+
+//verificando se o usuario pode acessar essa pagina
+verificarTipo();
+
+//Obter o id que sera excluido
+$idNoticia = $_GET['id'];
+$idUsuario = $_GET['id'];
+$tipoUsuario = $_GET['id'];
+
+// Chamar/executar a função que irá fazer o DELETE
+excluirNoticia($conexao, $idNoticia, $idUsuario, $tipoUsuario);
+
+//Redirecionamento para a página de usuário
+header("location:usuarios.php");
+
+?>
